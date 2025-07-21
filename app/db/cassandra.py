62c0416435a13b13d1a80a2d19432886790972b2
Ahmad Ameen <ahmadmameen7@gmail.com>
2025-07-21 21:27:30 +0100

    fix: cassandra session

--- app/db/cassandra.py
+++ app/db/cassandra.py
@@ -1,10 +1,23 @@
+import time
 from cassandra.cluster import Cluster
 
+
+
 def get_cassandra_session():
-    cluster = Cluster(['cassandra'])
-    session = cluster.connect()
-    session.set_keyspace('tochly')
-    return session
+    for _ in range(10):
+        try:
+            cluster = Cluster(['cassandra'])
+            return cluster.connect()
+        except Exception as e:
+            print("Cassandra not ready yet:", e)
+            time.sleep(5)
+        raise RuntimeError("Cassandra failed after multiple retries")
+
+# def get_cassandra_session():
+#     cluster = Cluster(['cassandra'])
+#     session = cluster.connect()
+#     session.set_keyspace('tochly')
+#     return session
 
 # Global session instance
 session = get_cassandra_session()
